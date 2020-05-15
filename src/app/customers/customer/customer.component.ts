df8199952e1c16d163a1d4a54d696432b03785e3
shahrukk <shahrukh.c.khan@capgemini.com>
2020-05-15 15:32:57 +0530

    Template driven form validation

---
+++ src/app/customers/customer/customer.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Customer } from '../customer';
+import { NgForm } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-customer',
+  templateUrl: './customer.component.html',
+  styleUrls: ['./customer.component.css']
+})
+export class CustomerComponent implements OnInit {
+  customer = new Customer();
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  save(customerForm: NgForm) {
+    console.log(customerForm.form);
+    console.log('Saved: ' + JSON.stringify(customerForm.value));
+  }
+}
